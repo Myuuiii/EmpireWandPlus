bc6713ccbc7cf1e0475329314a8671e4517bb66f
Myuu <mute@mutedevs.nl>
2022-01-21 20:48:17 +0100

    Updated Wand Spell List Formatting

--- src/EmpireWandPlus/src/main/java/moe/myuuiii/empirewandplus/WandSpellLists.java
+++ src/EmpireWandPlus/src/main/java/moe/myuuiii/empirewandplus/WandSpellLists.java
@@ -5,18 +5,27 @@ import java.util.List;
 
 public class WandSpellLists {
 	public static final List<String> EmpireWandSpells = Arrays.asList(new String[] {
-			Spells.Launch, Spells.Fireball, Spells.EmpireComet, Spells.EmpireSpark, Spells.EmpireCloud
+			Spells.Launch,
+			Spells.Fireball,
+			Spells.EmpireComet,
+			Spells.EmpireSpark,
+			Spells.EmpireCloud
 	});
 
 	public static final List<String> BloodWandSpells = Arrays.asList(new String[] {
-			Spells.BloodSpark, Spells.BloodWave, Spells.BloodCloud
+			Spells.BloodSpark,
+			Spells.BloodWave,
+			Spells.BloodCloud
 	});
 
 	public static final List<String> PoisonScytheWandSpells = Arrays.asList(new String[] {
-			Spells.PoisonSpark, Spells.PoisonWave, Spells.PoisonCloud
+			Spells.PoisonSpark,
+			Spells.PoisonWave,
+			Spells.PoisonCloud
 	});
 
 	public static final List<String> CelestialWandSpells = Arrays.asList(new String[] {
-			Spells.Lightning, Spells.CelestialCloud
+			Spells.Lightning,
+			Spells.CelestialCloud
 	});
 }
