294dadeb40475a3a1dd43104ceb9ee163183c8ce
Myuu <mute@mutedevs.nl>
2022-01-27 23:49:40 +0100

    Partial Data.java #83

--- src/EmpireWandPlus/src/main/java/moe/myuuiii/empirewandplus/Data.java
+++ src/EmpireWandPlus/src/main/java/moe/myuuiii/empirewandplus/Data.java
@@ -11,52 +11,51 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Data {
-	public static List<String> wandTypes = Arrays
+	public static final List<String> wandTypes = Arrays
 			.asList(new String[] { "empire", "blood", "scythe", "celestial", "hell" });
 
-	public static String empireWandName = ChatColor.GOLD + "Empire wand";
-	public static String empireWandConfigurationName = "EmpireWand";
+	public static final String empireWandName = ChatColor.GOLD + "Empire wand";
+	public static final String empireWandConfigurationName = "EmpireWand";
 
-	public static String bloodWandName = ChatColor.RED + "Blood wand";
-	public static String bloodWandConfigurationName = "BloodWand";
+	public static final String bloodWandName = ChatColor.RED + "Blood wand";
+	public static final String bloodWandConfigurationName = "BloodWand";
 
-	public static String scytheWandName = ChatColor.GREEN + "Poison scythe wand";
-	public static String scytheWandConfigurationName = "ScytheWand";
+	public static final String scytheWandName = ChatColor.GREEN + "Poison scythe wand";
+	public static final String scytheWandConfigurationName = "ScytheWand";
 
-	public static String celestialWandName = ChatColor.LIGHT_PURPLE + "Celestial Wand";
-	public static String celestialWandConfigurationName = "CelestialWand";
+	public static final String celestialWandName = ChatColor.LIGHT_PURPLE + "Celestial Wand";
+	public static final String celestialWandConfigurationName = "CelestialWand";
 
-	public static String hellWandName = ChatColor.RED + "Hell Wand";
-	public static String hellWandConfigurationName = "HellWand";
+	public static final String hellWandName = ChatColor.RED + "Hell Wand";
+	public static final String hellWandConfigurationName = "HellWand";
 
-	public static String prefix = ChatColor.GRAY + "[" + ChatColor.LIGHT_PURPLE + "X" + ChatColor.GRAY + "] "
-			+ ChatColor.RESET;
-	public static String currentSpellMessage = ChatColor.GRAY + "Current Spell: " + ChatColor.LIGHT_PURPLE;
-	public static String unknownSpellMessage = ChatColor.GRAY
-			+ "The current spell was not recognized, maybe you have not selected one yet?";
-	public static String wandNotImplementedYet = "This wand has not yet been implemented";
+	public static final String prefix = Extensions.colorText("&7[&dX&7]&r ");
+	public static final String currentSpellMessage = Extensions.colorText("&7Current Spell: &d");
+	public static final String unknownSpellMessage = Extensions
+			.colorText("&7The current spell was not recognized, maybe you have not selected one yet?");
+	public static final String wandNotImplementedYet = "This wand has not yet been implemented";
 
-	public static String wandDisabled = ChatColor.RED + "This wand has been disabled";
+	public static final String wandDisabled = Extensions.colorText("&cThis wand has been disabled");
 
-	public static ArrayList<Snowball> fireballs = new ArrayList<>();
-	public static ArrayList<Snowball> firepulses = new ArrayList<>();
+	public static final ArrayList<Snowball> fireballs = new ArrayList<>();
+	public static final ArrayList<Snowball> firepulses = new ArrayList<>();
 
-	public static ArrayList<Snowball> lightningBolts = new ArrayList<>();
-	public static ArrayList<Snowball> smites = new ArrayList<>();
+	public static final ArrayList<Snowball> lightningBolts = new ArrayList<>();
+	public static final ArrayList<Snowball> smites = new ArrayList<>();
 
-	public static ArrayList<Snowball> empireComets = new ArrayList<>();
+	public static final ArrayList<Snowball> empireComets = new ArrayList<>();
 
-	public static ArrayList<UUID> bloodCloudUsers = new ArrayList<>();
-	public static ArrayList<UUID> celestialCloudUsers = new ArrayList<>();
-	public static ArrayList<UUID> empireCloudUsers = new ArrayList<>();
-	public static ArrayList<UUID> poisonCloudUsers = new ArrayList<>();
+	public static final ArrayList<UUID> bloodCloudUsers = new ArrayList<>();
+	public static final ArrayList<UUID> celestialCloudUsers = new ArrayList<>();
+	public static final ArrayList<UUID> empireCloudUsers = new ArrayList<>();
+	public static final ArrayList<UUID> poisonCloudUsers = new ArrayList<>();
 
-	public static ArrayList<Snowball> poisonWaves = new ArrayList<>();
-	public static ArrayList<UUID> poisonUsers = new ArrayList<>();
+	public static final ArrayList<Snowball> poisonWaves = new ArrayList<>();
+	public static final ArrayList<UUID> poisonUsers = new ArrayList<>();
 
-	public static ArrayList<Snowball> bloodWaves = new ArrayList<>();
-	public static ArrayList<UUID> bloodUsers = new ArrayList<>();
+	public static final ArrayList<Snowball> bloodWaves = new ArrayList<>();
+	public static final ArrayList<UUID> bloodUsers = new ArrayList<>();
 
-	public static ArrayList<Snowball> flameWaves = new ArrayList<>();
-	public static ArrayList<UUID> flameUsers = new ArrayList<>();
+	public static final ArrayList<Snowball> flameWaves = new ArrayList<>();
+	public static final ArrayList<UUID> flameUsers = new ArrayList<>();
 }
